03b69fd45c63f2cfebb21f2a69758b1a3d3445e0
OharaKohei <gofue0723@gmail.com>
2015-06-20 16:22:01 +0900

    [add] 役職としてPostモデルを追加. Customerにpost_idを外部キーとして追加.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620060439) do
+ActiveRecord::Schema.define(version: 20150620071928) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -39,14 +39,22 @@ ActiveRecord::Schema.define(version: 20150620060439) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "company_id"
+    t.integer  "post_id"
   end
 
   add_index "customers", ["company_id"], name: "index_customers_on_company_id"
   add_index "customers", ["email"], name: "index_customers_on_email", unique: true
+  add_index "customers", ["post_id"], name: "index_customers_on_post_id"
 
   create_table "participants", force: :cascade do |t|
     t.string   "name"
     t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
