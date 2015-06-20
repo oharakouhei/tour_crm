e8534ed140407b437cc4989bc1b76ff5e59a3221
OharaKohei <gofue0723@gmail.com>
2015-06-20 09:00:48 +0900

    [add] validationの追加.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
   # participantとの関係は？
   has_many :participants
+  validates :guide_name,
+    presence: { message: 'は必須です' },
+    length: { maximum: 20, message: '%{value}は%{count}文字以下でなければなりません' }
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,5 @@
 class Participant < ActiveRecord::Base
   # courseとの関係は？
   belongs_to :course
+  validates :name, presence: true, length: { maximum: 20 }
 end
