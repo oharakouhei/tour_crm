c9ab7ca8ad330e8d392842db211a341aae2493ec
OharaKohei <gofue0723@gmail.com>
2015-06-20 16:16:39 +0900

    [add] CustomerにおいてCompanyを選択できるよう修正. パーシャルのlocal変数の渡し方, f.collection_selectに注意

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,6 +10,8 @@ class Customer < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email,
       presence: true, format: { with: VALID_EMAIL_REGEX }
+    validates :company_id,
+      presence: true
 
     def full_name
         full_name = family_name + given_name
