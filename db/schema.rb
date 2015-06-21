5760db9ac6e43f41343dd9e86991aeca187b49c7
OharaKohei <gofue0723@gmail.com>
2015-06-21 17:41:06 +0900

    [add] userテーブルにimage_urlカラムを追加.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621073845) do
+ActiveRecord::Schema.define(version: 20150621084013) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20150621073845) do
     t.datetime "updated_at",                          null: false
     t.string   "family_name"
     t.string   "given_name"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
