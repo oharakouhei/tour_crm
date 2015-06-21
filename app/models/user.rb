43aff627cd0b00c27116a5b2b7d42b305bb6dabf
OharaKohei <gofue0723@gmail.com>
2015-06-21 17:03:03 +0900

    [add] commentsテーブルにuser_idを追加.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_one :comment
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :family_name,
+    presence: true
+  validates :given_name,
+    presence: true
 end
